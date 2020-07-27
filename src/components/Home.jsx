1b1453e4124c952b8bbeda237340d033f718fabf
Connor Rose <connorrose.git@gmail.com>
2020-07-27 15:08:36 -0700

    feat: add collection logic inc. pieces to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 import CollectionPanel from './CollectionPanel';
 
-const Home = () => (
-  <CollectionPanel
-    gameList={[
-      {
-        img:
-          'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559258096678-51Eiofu9mqL.jpg',
-        title: 'Catan',
-        id: 'OIXt3DmJU0',
-      },
-      {
-        img:
-          'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559258096678-51Eiofu9mqL.jpg',
-        title: 'Catan',
-        id: 'OIXt3DmJU2',
-      },
-    ]}
-  />
-);
+const Home = () => {
+  // const testArr = [
+  //   {
+  //     img:
+  //       'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559258096678-51Eiofu9mqL.jpg',
+  //     title: 'Catan',
+  //     id: 'OIXt3DmJU0',
+  //   },
+  //   {
+  //     img:
+  //       'https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254941010-61PJxjjnbfL.jpg',
+  //     title: 'Spirit Island',
+  //     id: 'kPDxpJZ8PD',
+  //   },
+  // ];
+
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(() => {
+    fetch('/collection')
+      .then((response) => response.json())
+      .then((data) => (Array.isArray(data) > 0 ? setGameList(data) : null))
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <>
+      <Link to="/games">
+        <Button variant="success">Search for games</Button>
+      </Link>
+      {gameList.length > 0 ? (
+        <CollectionPanel gameList={gameList} />
+      ) : (
+        <Alert variant="warning">Login to add some games to your collection!</Alert>
+      )}
+    </>
+  );
+};
 
 export default Home;
