b42d38a0f4df3be7976d9e1ebb2b4636042bd83c
Steven_Nguyen <stevennnn13@gmail.com>
2020-07-26 19:26:35 -0700

    Fixed fetch query and updated line breaks

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -3,22 +3,10 @@ const db = require('../models/model');
 
 const gameController = {};
 
-gameController.getHaveMissingPieces = (req, res, next) => {
-  const getMissingQuery = {
-    text:
-      'SELECT u.username, g.name, h.* FROM have_missing_pieces AS h JOIN game AS g ON g._id = h.game_id JOIN users AS u ON u._id = h.users_id WHERE h.game_id = $1',
-    values: [req.body.game.id],
-  };
-  db.query(getMissingQuery)
-    .then((data) => {
-      res.locals.have_missing_pieces = data.rows[0];
-      next();
-    })
-    .catch((error) => next({ error }));
-};
-
 gameController.getGameInfo = (req, res, next) => {
-  fetch('https://www.boardgameatlas.com/api/search?name=Cata&client_id=JLBr5npPhV&fuzzy_match=true')
+  fetch(
+    `https://www.boardgameatlas.com/api/search?name=${req.query.query}&client_id=JLBr5npPhV&fuzzy_match=true`,
+  )
     .then((data) => data.json())
     .then((body) => {
       res.locals.games = body.games.map(
--- server/routes/gameRouter.js
+++ server/routes/gameRouter.js
@@ -3,10 +3,6 @@ const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/gameController');
 
-router.get('/missing_pieces', gameController.getHaveMissingPieces, (req, res, next) => {
-  res.status(200).json(res.locals.have_missing_pieces);
-});
-
 router.get('/', gameController.getGameInfo, (req, res) => {
   res.status(200).json(res.locals.games);
 });
