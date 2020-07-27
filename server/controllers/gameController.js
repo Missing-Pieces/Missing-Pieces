9b5d55faceb38a7120db24f81c7c00d0f1396378
Steven_Nguyen <stevennnn13@gmail.com>
2020-07-27 10:23:07 -0700

    need to fix addParts

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -23,4 +23,37 @@ gameController.getGameInfo = (req, res, next) => {
     });
 };
 
+gameController.fetchGameById = (req, res, next) => {
+  fetch(
+    `https://www.boardgameatlas.com/api/search?ids=${res.locals.collection.game_id}&pretty=true&client_id=JLBr5npPhV`,
+  )
+    .then((data) => data.json())
+    .then((body) => {
+      const { name, id, year_published, images, description, primary_publisher } = body.games[0];
+      res.locals.game = {
+        name,
+        id,
+        year_published,
+        img: images.original,
+        description,
+        primary_publisher,
+      };
+      return next();
+    });
+};
+
+gameController.addGame = (req, res, next) => {
+  const { name, id, year_published, img, description, primary_publisher } = res.locals.game;
+  const addGameQuery = {
+    text:
+      'INSERT INTO public.game(name, _id, years_published, images, description, primary_publisher ) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+    values: [name, id, year_published, img, description, primary_publisher],
+  };
+  db.query(addGameQuery, (err, results) => {
+    console.log(results);
+    if (err) return next(err);
+    console.log('Game has successfully been added');
+    return next();
+  });
+};
 module.exports = gameController;
--- server/routes/collectionRouter.js
+++ server/routes/collectionRouter.js
@@ -2,5 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const collectionController = require('../controllers/collectionController');
+const gameController = require('../controllers/gameController');
+
+router.post(
+  '/:game_id',
+  collectionController.addCollection,
+  gameController.fetchGameById,
+  gameController.addGame,
+  (req, res) => {
+    res.status(200).json(res.locals.game);
+  },
+);
 
 module.exports = router;
